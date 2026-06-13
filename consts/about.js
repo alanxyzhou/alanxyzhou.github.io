eaf1365c98b0577b5f443862827523db87c788e9
Alan Zhou <30447373+alanxyzhou@users.noreply.github.com>
2026-06-12 23:05:48 -0700

    aboot

---
+++ consts/about.js
@@ -0,0 +1,14 @@
+export const aboutTitle = "Who is Megameter?";
+
+export const aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin libero.";
+
+export const prospectusImages = [
+  "a07_image.jpg",
+  "a08_image.jpg",
+  "a09_image.jpg",
+  "a10_image.jpg",
+  "a11_image.jpg",
+  "a12_image.jpg",
+  "a13_image.jpg",
+  "a15_image.jpg"
+];
